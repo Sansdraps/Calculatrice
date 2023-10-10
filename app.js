0fd3b13cf5a07af9e88db78eb34fecde870a2591
Sansdraps <sandra.letessier@gmail.com>
2023-10-10 11:44:40 +0200

    récupération des touches) git push

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // DOM
 const touches = [...document.querySelectorAll('.bouton')];
-console.log(touches);
+const listeKeycode = touches.map(touche => touche.dataset.key)
+// console.log(listeKeycode);
+
+document.addEventListener('keydown', (e) => {
+  const valeur = e.keyCode.toString;
+})
+
+document.addEventListener('click', (e) => {
+  // console.log(e);
+  const valeur = e.target;
+  console.log(valeur);
+})
